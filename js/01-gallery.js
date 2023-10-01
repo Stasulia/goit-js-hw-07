e83f537deadc3bc5fdea44f18af10b2f1a100661
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-10-01 21:50:12 +0300

    1 task is done

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+
+const container = document.querySelector('.gallery');
+container.addEventListener('click', imageClick);
+
+const images = galleryItems
+.map (({preview, original, description}) => `
+<li class = "gallery__item">
+<a class = "gallery__link" href='${original}'>
+<img loading = 'lazy' class = "gallery__image" src='${preview}' data-source = '${original}' alt = '${description}'/>
+</a>
+</li>`
+)
+.join(" ");
+
+container.insertAdjacentHTML('afterbegin', images);
+
+function imageClick(event){
+    event.preventDefault();
+ if (event.target.nodeName !== "IMG") {
+    return;
+}
+const originalImage = event.target.dataset.source;
+const originalAlt = event.target.getAttribute('alt');
+
+const instance = basicLightbox.create(`
+  <img src="${originalImage}" alt="${originalAlt}">
+`);
+instance.show();
+
+document.addEventListener('keydown', closeInstance);
+
+function closeInstance(evt) {
+    if (evt.code === "Escape") {
+        instance.close();
+        document.removeEventListener('keydown', closeInstance);
+    }
+}
+}
+
