214462d331da7adc54e2051b02d30cb9e6ef19d6
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-10-02 17:41:55 +0300

    Summary

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+const container = document.querySelector('.gallery');
+
+const images = galleryItems
+.map(({preview, original, description}) => ` 
+<li class = 'gallery__item'>
+    <a class = "gallery__link" href="${original}">
+      <img loading = 'lazy' class = "gallery__image" src=" ${preview} " alt=" ${description} " />
+    </a>
+</li> `
+)
+.join(" ");
+
+container.insertAdjacentHTML('afterbegin', images);
+
+const instance = new SimpleLightbox('.gallery a',{
+ captionsData: 'alt',
+ captionDelay: '250',
+});
+
